2bc145ad63ea2cee91d9793b5a3cc80dafa22510
CHH3213 <1213472983@qq.com>
2023-07-22 15:32:45 +0800

    fix 模型加载失败

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="E:\CHH3213_KING\NO_Chinese\stable-diffusion-webui\venv\Scripts\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --opt-sdp-no-mem-attention --listen --enable-insecure-extension-access
+set COMMANDLINE_ARGS=--xformers --opt-sdp-no-mem-attention --listen --enable-insecure-extension-access --medvram --autolaunch --deepdanbooru --no-gradio-queue
 
 call webui.bat
